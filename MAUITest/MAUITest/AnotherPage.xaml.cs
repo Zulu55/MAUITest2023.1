01787546c1394581a755706205b00bedf0e0c01e
Juan Zuluaga <Juan.Zuluaga_EX@kdrp.com>
2023-04-12 19:16:41 -0500

    Content page demo

---
+++ MAUITest/MAUITest/AnotherPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MAUITest;
+
+public partial class AnotherPage : ContentPage
+{
+	public AnotherPage()
+	{
+		InitializeComponent();
+	}
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		Navigation.PopAsync();
+    }
+}
---
+++ MAUITest/MAUITest/ContentPageDemo.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MAUITest;
+
+public partial class ContentPageDemo : ContentPage
+{
+	public ContentPageDemo()
+	{
+		InitializeComponent();
+	}
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		Navigation.PushAsync(new AnotherPage());
+    }
+}
