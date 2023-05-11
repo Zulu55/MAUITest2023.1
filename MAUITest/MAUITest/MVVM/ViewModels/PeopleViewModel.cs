390a478cba3f2ff40565d0545ce75c2c3ddedf1a
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-05-10 19:36:47 -0500

    Value Converters

--- MAUITest/MAUITest/MVVM/ViewModels/PeopleViewModel.cs
+++ MAUITest/MAUITest/MVVM/ViewModels/PeopleViewModel.cs
@@ -11,10 +11,10 @@ namespace MAUITest.MVVM.ViewModels
                 new Person { Name = "Juan", Age = 48, BirthDate = new DateTime(1974, 9, 23), IsMarried =true, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
                 new Person { Name = "Ledys", Age = 38, BirthDate = new DateTime(1974, 9, 23), IsMarried =true, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
                 new Person { Name = "Geralin", Age = 28, BirthDate = new DateTime(1974, 9, 23), IsMarried =true, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
-                new Person { Name = "Ronal", Age = 18, BirthDate = new DateTime(1974, 9, 23), IsMarried =true, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
-                new Person { Name = "Valery", Age = 8, BirthDate = new DateTime(1974, 9, 23), IsMarried =true, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
+                new Person { Name = "Ronal", Age = 18, BirthDate = new DateTime(1974, 9, 23), IsMarried =false, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
+                new Person { Name = "Valery", Age = 8, BirthDate = new DateTime(1974, 9, 23), IsMarried =false, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
                 new Person { Name = "Omaira", Age = 58, BirthDate = new DateTime(1974, 9, 23), IsMarried =true, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
-                new Person { Name = "Martiana", Age = 18, BirthDate = new DateTime(1974, 9, 23), IsMarried =true, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
+                new Person { Name = "Martiana", Age = 18, BirthDate = new DateTime(1974, 9, 23), IsMarried =false, LunchTime = new TimeSpan(12, 30, 0), Weight = 89 },
             };
         }
 
