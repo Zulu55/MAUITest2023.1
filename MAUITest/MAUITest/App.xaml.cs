b7de7458ecfe8418f4ee95d12e764ce5574d49bd
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-05-12 12:30:09 -0500

    Styles

--- MAUITest/MAUITest/App.xaml.cs
+++ MAUITest/MAUITest/App.xaml.cs
@@ -7,7 +7,7 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        MainPage = new NavigationPage(new DemoAutoPropertyChangedView());
+        MainPage = new NavigationPage(new StyleDemoView());
     }
 }
 
---
+++ MAUITest/MAUITest/MVVM/Views/StyleDemoView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUITest.MVVM.Views;
+
+public partial class StyleDemoView : ContentPage
+{
+	public StyleDemoView()
+	{
+		InitializeComponent();
+	}
+}
